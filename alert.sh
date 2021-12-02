cd16462dec9a20306001795f3adb631a68779021
Rotherda <70058769+rotherda@users.noreply.github.com>
2021-12-02 21:10:59 +0300

    Update alert.sh

--- alert.sh
+++ alert.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 while true; do
-	timeout 300s tcpdump -nnSX port 80 | grep HTTP/1.1.4* | grep HTTP/1.1.5* > /var/log/http.txt
+	timeout 300s tcpdump -nnSX port 80 | grep HTTP/1.1.404 > /var/log/http.txt
 	log=/var/log/http.txt
 	if [ -n $log ]; then
 		zaman=$(date)
-		echo $zaman 404 Found >> /var/log/alert.txt
+		echo $zaman ERROR FOUND >> /var/log/alert.txt
 		continue
 	else
 		continue
