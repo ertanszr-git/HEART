ee508d7ade549b5c15bce35516eebb4ff473f1ec
Rotherda <70058769+rotherda@users.noreply.github.com>
2021-12-02 19:19:05 +0300

    Update alert.sh

--- alert.sh
+++ alert.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-
 while true; do
-	tcpdump -nnSX port 80 | grep HTTP/1.1.404 > /var/log/http.txt
+	timeout 300s tcpdump -nnSX port 80 | grep HTTP/1.1.404 > /var/log/http.txt
 	log=/var/log/http.txt
-	#if kısmının 5 dkda bir tetiklenmesi lazım.
 	if [ -n $log ]; then
 		zaman=$(date)
-		echo $zaman 404 Bulundu >> /home/kali/alert.txt
+		echo $zaman 404 Bulundu >> /var/log/alert.txt
+		continue
 	else
 		continue
 	fi
+	
 done
