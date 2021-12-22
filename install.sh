ee690211b7c1976c9215b0166978a418160bc1ef
Rotherda <70058769+rotherda@users.noreply.github.com>
2021-12-22 21:59:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ cd /usr/share
 git clone https://github.com/rotherda/HEART
 cd HEART
 dos2unix heart.sh
-echo "00 20-23 * * 1-5 curl -F document=@"/var/log/alert.txt" https://api.telegram.org/bot5096290815:AAEY15l7FQGF5C35drLoA2nS60RU-yYz1h8/sendDocument?chat_id=-1001711865343"
+
+echo curl -F document=@"/var/log/alert.txt" https://api.telegram.org/bot5096290815:AAEY15l7FQGF5C35drLoA2nS60RU-yYz1h8/sendDocument?chat_id=-1001711865343
