f9d8e0f95ea836ff3021ac4e20eb56b474f9f434
Rotherda <ertanszr123@gmail.com>
2021-12-22 20:45:02 +0300

    update

--- heart.sh
+++ heart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 while true; do
-	timeout 60s tcpdump -nnSX port 80 | grep HTTP/1.1.404 > /var/log/http.txt
+	timeout $looptime tcpdump -nnSX port 80 | grep HTTP/1.1.404 > /var/log/http.txt
 	log=/var/log/http.txt
 	if [ -n $log ]; then
 		zaman=$(date)
