da3f77c62db7ed0ffd8b88e90a8fdaab43749912
Ertan Sözer <70058769+ertanszr-git@users.noreply.github.com>
2022-04-09 14:23:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo apt install tcpdump
 sudo apt install dos2unix
 cp heart /usr/bin
 cd /usr/bin/
-sudo chmod 777 heart
+sudo chmod 500 heart
 cd /usr/share
 git clone https://github.com/rotherda/HEART
 cd HEART
